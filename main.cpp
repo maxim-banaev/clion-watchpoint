e82a038b1212fd07048a21d6afce811e951622dd
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2019-08-09 12:38:15 +0300

    some updates

--- main.cpp
+++ main.cpp
@@ -1,58 +1,57 @@
-#include <iostream>
+#include "test.h"
 #include <array>
 #include <vector>
-#include "test.h"
 
 struct customType {
 public:
-    int x;
+  int x;
 };
 
 int main() {
-    customType type{1};
-    std::string string;
-    std::vector<int> vector;
-    std::array<int, 1> array = {1};
-    int int_ = static_cast<int>(1.2);
-    bool bool_ = true;
+  customType type{1};
+  std::string string_;
+  std::vector<int> vector_ = {0};
+  std::array<int, 1> array_ = {1};
+  int int_ = static_cast<int>(1.2);
+  bool bool_ = true;
 
-    int tmp = 0;
+  int tmp = 0;
 
-    auto l = [&](int lamdba_param) {
-        int_ = lamdba_param;
-    };
+  auto l = [&](int lamdba_param) { int_ = lamdba_param; };
 
-    // change outside current frame
-    change(int_);
-    change(string);
+  // change outside current frame
+  change(int_);
+  change(string_);
+  change(bool_);
+  change(array_);
+  change(vector_);
 
-    // int read
-    tmp = int_;
+  // int read
+  tmp = int_;
 
-    // bool read
-    tmp = (int) bool_;
+  // bool read
+  tmp = (int)bool_;
 
-    // array read
-    tmp = array[0];
-    // array write
-    array[0] = 2;
+  // array read
+  tmp = array_[0];
+  // array write
+  array_[0] = 2;
 
-    // vector read
-    tmp = vector.size();
-    // vector write
-    vector.push_back(1);
+  // vector read
+  tmp = vector_.size();
+  // vector write
+  vector_.push_back(1);
 
-    // string read
-    tmp = string.length();
-    // string write
-    string = "new string";
+  // string read
+  tmp = string_.length();
+  // string write
+  string_ = "new string";
 
-    // custom read
-    tmp = type.x;
-    // custom write
-    type.x = 2;
+  // custom read
+  tmp = type.x;
+  // custom write
+  type.x = 2;
 
-    l(12);
-    return 0;
+  l(12);
+  return 0;
 }
-
--- test.h
+++ test.h
@@ -1,7 +1,13 @@
-void change(int &i) {
-    i = 12;
-}
+#include <array>
+#include <string>
+#include <vector>
 
-void change(std::string &s) {
-    s = "changed";
-}
+void change(int &i) { i = 12; }
+
+void change(bool &b) { b = false; }
+
+void change(std::string &s) { s = "changed"; }
+
+void change(std::array<int, 1> &arr) { arr[0] = 0; }
+
+void change(std::vector<int> &vect) { vect.push_back(1); }
